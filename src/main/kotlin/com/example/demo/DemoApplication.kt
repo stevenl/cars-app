f8098a06e3d79dd1194883cea958d95d7fc3bccf
Steven Lee <stevenwh.lee@gmail.com>
2024-02-16 20:21:08 +1100

    Add logging

--- src/main/kotlin/com/example/demo/DemoApplication.kt
+++ src/main/kotlin/com/example/demo/DemoApplication.kt
@@ -1,11 +1,16 @@
 package com.example.demo
 
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
 class DemoApplication
 
+val logger: Logger = LoggerFactory.getLogger(DemoApplication::class.java)
+
 fun main(args: Array<String>) {
     runApplication<DemoApplication>(*args)
+    logger.info("Application started")
 }
