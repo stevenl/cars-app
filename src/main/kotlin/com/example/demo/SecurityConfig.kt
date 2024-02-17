90d00fa7c4a17e014808adfb59e943711c86c1f9
Steven Lee <stevenwh.lee@gmail.com>
2024-02-17 23:14:15 +1100

    Add a CORS filter

--- src/main/kotlin/com/example/demo/SecurityConfig.kt
+++ src/main/kotlin/com/example/demo/SecurityConfig.kt
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpMethod
 import org.springframework.security.authentication.AuthenticationManager
+import org.springframework.security.config.Customizer
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
@@ -15,6 +16,10 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.CorsConfigurationSource
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
+
 
 @Configuration
 @EnableWebSecurity
@@ -39,6 +44,7 @@ class SecurityConfig(
     @Bean
     fun filterChain(http: HttpSecurity): SecurityFilterChain =
         http.csrf { it.disable() }
+            .cors(Customizer.withDefaults())
             .sessionManagement {
                 // never create a session, and therefore we can disable CSRF
                 it.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
@@ -57,4 +63,18 @@ class SecurityConfig(
                 it.authenticationEntryPoint(exceptionHandler)
             }
             .build()
+
+    @Bean
+    fun corsConfigurationSource(): CorsConfigurationSource {
+        val config = CorsConfiguration().apply {
+            allowedOrigins = listOf("*")
+            allowedMethods = listOf("*")
+            allowedHeaders = listOf("*")
+            allowCredentials = false
+            applyPermitDefaultValues()
+        }
+        return UrlBasedCorsConfigurationSource().apply {
+            registerCorsConfiguration("/**", config)
+        }
+    }
 }
