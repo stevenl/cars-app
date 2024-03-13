b2aefe01660472280975c311c8627f46be46284b
Steven Lee <stevenwh.lee@gmail.com>
2024-03-13 17:50:17 +1100

    Use MUI DataGrid to generate the table

--- cars-frontend/src/components/CarList.tsx
+++ cars-frontend/src/components/CarList.tsx
@@ -1,6 +1,6 @@
-import {CarResponse} from "../types.ts";
 import {useQuery} from "@tanstack/react-query";
 import {getCars} from "../api/cars-api.ts";
+import {DataGrid, GridColDef} from "@mui/x-data-grid";
 
 function CarList() {
     const {data, error, isSuccess} = useQuery({
@@ -8,28 +8,26 @@ function CarList() {
         queryFn: getCars,
     })
 
+    const columns: GridColDef[] = [
+        {field: 'brand', headerName: 'Brand', width: 200},
+        {field: 'model', headerName: 'Model', width: 200},
+        {field: 'color', headerName: 'Color', width: 200},
+        {field: 'registrationNumber', headerName: 'Reg.nr', width: 150},
+        {field: 'modelYear', headerName: 'Model Year', width: 150},
+        {field: 'price', headerName: 'Price', width: 150},
+    ]
+
     if (!isSuccess) {
         return <span>Loading...</span>
     } else if (error) {
         return <span>Error when fetching cars...</span>
     }
     return (
-        <table>
-            <tbody>
-            {
-                data.map((car: CarResponse) =>
-                    <tr key={car._links.self.href}>
-                        <td>{car.brand}</td>
-                        <td>{car.model}</td>
-                        <td>{car.color}</td>
-                        <td>{car.registrationNumber}</td>
-                        <td>{car.modelYear}</td>
-                        <td>{car.price}</td>
-                    </tr>
-                )
-            }
-            </tbody>
-        </table>
+        <DataGrid
+            rows={data}
+            columns={columns}
+            getRowId={row => row._links.self.href}
+        />
     )
 }
 
