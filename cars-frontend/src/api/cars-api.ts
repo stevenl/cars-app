c8d2a471dd5ff2dd01e5f2eb7e26272e33d1bc20
Steven Lee <stevenwh.lee@gmail.com>
2024-03-21 23:19:08 +1100

    Provide authorization to the car api requests

--- cars-frontend/src/api/cars-api.ts
+++ cars-frontend/src/api/cars-api.ts
@@ -1,36 +1,38 @@
 import {Car, CarEntry, CarResponse} from "../types.ts";
-import axios from "axios";
+import axios, {AxiosRequestConfig} from "axios";
 
 const VITE_API_URL = import.meta.env.VITE_API_URL
 
 export const getCars = async (): Promise<CarResponse[]> => {
-    const response = await axios.get(`${VITE_API_URL}/api/cars`)
+    const response = await axios.get(`${VITE_API_URL}/api/cars`, getRequestConfig())
     return response.data._embedded.cars
 }
 
 export const addCar = async (car: Car): Promise<CarResponse> => {
     const response = await axios.post(
-        `${VITE_API_URL}/api/cars`, car, {
-            headers: {
-                'Content-Type': "application/json",
-            },
-        },
+        `${VITE_API_URL}/api/cars`, car, getRequestConfig()
     )
     return response.data
 }
 
 export const updateCar = async (carEntry: CarEntry): Promise<CarResponse> => {
     const response = await axios.put(
-        carEntry.url, carEntry.car, {
-            headers: {
-                'Content-Type': "application/json",
-            },
-        }
+        carEntry.url, carEntry.car, getRequestConfig()
     )
     return response.data
 }
 
 export const deleteCar = async (link: string): Promise<CarResponse> => {
-    const response = await axios.delete(link)
+    const response = await axios.delete(link, getRequestConfig())
     return response.data
+}
+
+function getRequestConfig(): AxiosRequestConfig {
+    const token =  sessionStorage.getItem("jwt")
+    return {
+        headers: {
+            'Content-Type': "application/json",
+            Authorization: token,
+        }
+    }
 }
